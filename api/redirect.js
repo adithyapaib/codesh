bab2bad009b7976916f4bf0747fca263bef95be2
Adithya Pai B <paiadithya26@gmail.com>
2021-11-11 21:05:08 +0530

    Added star page

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,14 @@
 import express from 'express';
 const app = express(); 
 import mongoose from "mongoose";
-import Model from "../models/Model";
+import Model from "../models/Model";    
+app.set('view engine', 'ejs');
 require("dotenv").config();
+app.get('/:id', async(req, res) => {
+    await mongoose.connect(process.env.DB)
+    let id = await req.params.id;
+  let document = await Model.findOne({ username: id });
+  document ?  await res.render("code", { code :document.value, username: id }): await res.render("404")
+});
 
-
+export default app;
