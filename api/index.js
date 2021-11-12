ee81f712995e8df8fd352061ee83d81efada9f07
Adithya Pai B <paiadithya26@gmail.com>
2021-11-12 18:38:32 +0530

    Changed view to related path

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import  express from "express";
 const app = express();
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.status(200).render('index'))
+app.get('/', (req, res) => res.status(200).render('../views/index.ejs'))
 export default app;
