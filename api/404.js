d372fb316640c0d71491ec8cd3f953d6d4465c95
Adithya Pai B <paiadithya26@gmail.com>
2021-11-13 19:47:43 +0530

    Update 404.js

--- api/404.js
+++ api/404.js
@@ -1,5 +1,4 @@
-import  express from "express";
-const app = express();
+const app   require("express")();
 app.set('view engine', 'ejs');
 app.get('/404', (req, res) => res.render('404'))
 export default app;
