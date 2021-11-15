53bd753f9bef8e4d95fdf6f9e768dd2bc015a8cb
Adithya Pai B <paiadithya26@gmail.com>
2021-11-15 21:37:59 +0530

    Removed unwanted route

--- api/52.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-require("dotenv").config();
-app.get('/52', function (req, res) {
-    res.json(process.env.DB);
-    }
-);
-
-export default app;
