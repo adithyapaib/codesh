74095b7c5d5fb100dbdbc7a0c3701c3c2050abf7
Adithya Pai B <paiadithya26@gmail.com>
2021-11-15 21:17:05 +0530

    expanded function

--- api/user.js
+++ api/user.js
@@ -1,4 +1,10 @@
 import mongoose from "mongoose";
 import Model from "../models/Model";
 require("dotenv").config();
-export default async (req, res) => await mongoose.connect(process.env.DB) ? await (await Model.find({username: req.url.split('/user/')[1]})).length >0 ? res.status(200).json(true).end():res.status(200).json(false).end() : res.status(500).json(false).end();
+export default async (req, res) => {
+    await mongoose.connect(process.env.DB);
+    let a = await Model.find({username: req.url.split('/user/')[1]})
+    if ( a.length !=0 )
+     await res.status(200).json(true).end()
+    else await res.status(200).json(false).end() 
+}
