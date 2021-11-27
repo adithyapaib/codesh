b56f8f875f564fa710afba32d3cf94efa1ad4b65
Adithya Pai B <paiadithya26@gmail.com>
2021-11-27 06:48:38 +0530

    Updated 404 page

--- api/404.js
+++ api/404.js
@@ -1,4 +1,2 @@
-const app   require("express")();
-app.set('view engine', 'ejs');
-app.get('/404', (req, res) => res.render('404'))
-export default app;
+import a from './html/404'
+export default require('express')().get('/', (req, res) => res.status(200).send(a).end())
